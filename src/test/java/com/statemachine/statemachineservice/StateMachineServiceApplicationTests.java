c01cbf01d46dcd90fec08df2ef51bc0df3a20df5
math-ferreira <mat.s.ferreira@gmail.com>
2024-01-21 11:04:42 +0000

    feat: first commit

---
+++ src/test/java/com/statemachine/statemachineservice/StateMachineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statemachine.statemachineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateMachineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
